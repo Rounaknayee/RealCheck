db25ff06203ae160bad55c513fee20c426d54f9b
Rounak Nayee <rounaknayee@Rounaks-MacBook-Air.local>
2023-11-03 07:58:36 -0400

    db, config, routers, user models, middleware created

---
+++ backend/middleware/auth.js
@@ -0,0 +1,40 @@
+// middleware/auth.js
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const config = require('../config/config');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, config.jwtSecret);
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token
+    });
+
+    if (!user) {
+      throw new Error('User not found.');
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
+
+// Middleware to check user role
+const checkRole = (role) => (req, res, next) => {
+  if (req.user && req.user.role === role) {
+    next();
+  } else {
+    res.status(403).send('Error: Access denied.');
+  }
+};
+
+module.exports = {
+  auth,
+  checkRole
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,48 @@
+// server.js
+
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const config = require('./config/config');
+
+//Routes
+const userRouter = require('./routes/userRoutes');
+const supplierRouter = require('./routes/supplierRoutes');
+const manufacturerRouter = require('./routes/manufacturerRoutes');
+const publicRouter = require('./routes/publicRoutes');
+
 const app = express();
 app.use(cors());
-const PORT = 5001;
+// Async function to connect to DB and then start the server
+const startServer = async () => {
+  try {
+    // Await the connection to the database before starting the server
+    await connectDB();
+    app.listen(config.port, () => {
+      console.log(`Server is running on http://localhost:${config.port} `);
+    });
+  } catch (err) {
+    console.error("Database connection failed", err);
+    process.exit(1); // Exit with failure if the database connection fails
+  }
+};
 
-app.get('/api', (req, res) => {
-  res.json({ message: "Hello from the backend!" });
-});
+startServer(); // start the server
+// Diffrent Routes after starting server
 
-app.listen(PORT, () => {
-  console.log(`Rounak's Node.JS Server is running on http://localhost:${PORT}`);
+app.use('/api/users',userRouter);
+app.use('/api/suppliers',supplierRouter);
+app.use('/api/manufacturers',manufacturerRouter);
+// Public routes which are not protected by auth middleware
+app.use('/api/public', publicRouter);
+
+
+// app.use('/api/products',productRouter);
+// app.use('/api/orders',orderRouter);
+// app.use('/api/transactions',transactionRouter);
+
+
+// This is just a test api to check if the server is running
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Hello from the backend Worldddd!" });
 });
